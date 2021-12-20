1673d7ed19b8783120da6dcd99ab7aab51104b9d
Dominic Calleja <sgdcalle@student.liverpool.ac.uk>
2021-12-20 17:43:18 -0300

    some initial work on test script ij preporation of plotting

--- SRC/incident.py
+++ SRC/incident.py
@@ -9,28 +9,34 @@ class Incident():
     incident_severity = 1   # You can allocate self variables without the self. when you do it before the __init__
     __verbose__ = True # I always lik to have a __verbose__ variable in every class. When a variable has '__' at the begining and end that variable is private and only accessed from within the class 
 
-    def __init__(self, incident_id, date, time, location):
+    def __init__(self, platform_id, incident_id, date, latitude, longitude):
         self.incident_id = incident_id
+        self.platform_id = platform_id
         self.date = date
-        self.time = time
-        self.location = location
+        self.latitude = latitude
+        self.longitude = longitude
        
     def get_date(self):
         "Function to return the incident date"
         return self.date
 
-    def get_time(self):
+    def get_platform(self):
         "Function to return the "
-        return self.time
+        return self.platform_id
 
     def get_incident_severity(self):
         return self.incident_severity
 
     def gen_label(self):
-        label = '{}-{}{}-({})'.format(self.incident_id,self.date,self.time,self.location)
-        if self.__verbose__:  # This is an example of how to build your print statements. print statements are great to help you when running the code. But they can be annoying when you run the code in production so its a good idea to always use this structure
-            print(label)
-            
+        label = '{}-{}{}-({},{})'.format(self.platform_id, self.incident_id,
+                                      self.date, self.latitude,self.longitude)
+        return label 
+        # if self.__verbose__:  # This is an example of how to build your print statements. print statements are great to help you when running the code. But they can be annoying when you run the code in production so its a good idea to always use this structure
+        #     print(label)
+
+    def __repr__(self):
+        return '{} \n \t{}'.format('Incident Class :', self.gen_label())
+
 def print_incident(A):
     return A
 
--- test_1.py
+++ test_1.py
@@ -1,1 +1,50 @@
 # Test script
+
+"""
+Use this to do the development
+"""
+
+"""1. load data"""
+#use pandas to import  csv file
+import pandas as pd
+inc_df = pd.read_csv('Data/incident_data.csv')
+inc_df = inc_df.drop(columns='Unnamed: 0')
+
+inc_df.columns # Columns
+inc_df.index # Rows
+inc_df.loc[0,:] # first entry 
+
+
+inc_df.loc[:, 'COD_SONDA'] # column 
+inc_df.iloc[:, 0]
+
+inc_df.loc[0, 'COD_SONDA'] 
+
+
+"""2. build incident objects"""
+from SRC.incident import Incident
+
+['COD_SONDA', 'NK_INC','DAT_PRIMEIRA_OBS', 'NUM_LATITUDE', 'NUM_LONGITUDE']
+
+
+test1 = Incident(*inc_df.loc[0, ['COD_SONDA', 'NK_INC',
+                                'DAT_PRIMEIRA_OBS', 'NUM_LATITUDE', 'NUM_LONGITUDE']].values)
+
+float(inc_df.loc[:, 'NUM_LATITUDE'].values[0].replace(',', '.'))
+
+def convert_loc_string(pos):
+    if isinstance(pos,str):
+        return float(pos.replace(',', '.'))
+    else:
+        return pos
+
+lat = []
+lon = []
+for i in inc_df.index:
+    lat.append(convert_loc_string(inc_df.loc[i, 'NUM_LATITUDE']))
+    lon.append(convert_loc_string(inc_df.loc[i, 'NUM_LONGITUDE']))
+
+inc_df['NUM_LATITUDE'] = lat
+inc_df['NUM_LONGITUDE'] = lon
+
+"""3. plot data"""
